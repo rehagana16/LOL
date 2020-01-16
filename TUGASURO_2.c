90031f8936fb33a0703adc346517501078bd8f38
unknown <rehaganak@gmail.com>
2020-01-16 23:31:45 +0700

    modified the first file

---
+++ TUGASURO_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double first,second;
+    int p, i;
+    printf("Enter an operator (+, -, *, d): \n"); /* note d= derivative*/
+    scanf("%c", &operator);
+
+    if (operator == 'd') {
+        printf("Pangkat berapa: \n");
+        scanf("%d", &p);
+        int angka[p],o;
+        for(i=0; i <= p; i=i+1)
+        {
+        	printf("koefisien dari pangkat ke %d= ", i);
+        	scanf("%d", &angka[i]);
+        }
